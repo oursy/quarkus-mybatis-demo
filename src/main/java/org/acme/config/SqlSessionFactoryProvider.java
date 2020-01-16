c8dbc4948df7937138c8e6d7aad1ba13615a9eaa
oursy <1971555566@qq.com>
2020-01-16 18:42:45 +0800

    Add xml support

--- src/main/java/org/acme/config/SqlSessionFactoryProvider.java
+++ src/main/java/org/acme/config/SqlSessionFactoryProvider.java
@@ -26,7 +26,6 @@ import org.springframework.util.StringUtils;
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Produces;
 import javax.inject.Inject;
-import javax.ws.rs.core.Context;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.HashSet;
